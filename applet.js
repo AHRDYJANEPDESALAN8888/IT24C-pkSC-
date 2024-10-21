0f761cf305a3c4c7b6006369ef022eb26ce11926
AHRDYJANEPDESALAN8888 <desalan.ahrdy08@gmail.com>
2024-10-21 08:42:26 +0800

    Added some changes

--- applet.js
+++ applet.js
@@ -12,7 +12,7 @@ class AppletCard {
             <div class="card-body">
                 <h5 class="card-title">${this.title}</h5>
                 <p class="card-text">${this.description}</p>
-                <a href="${this.link}" class="btn btn-primary">Go to Applet</a>
+                <a href="${this.link}" class="btn btn-primary applet-btn" style="">Go to Applet</a>
             </div>
         `;
         return cardDiv;
@@ -20,26 +20,45 @@ class AppletCard {
 }
 
 class AppletRenderer {
-    constructor(containerId) {
+    constructor(containerId,searchInputId) {
         this.container = document.getElementById(containerId);
+
+        //
+        this.searchInput = document.getElementById(searchInputId);
+        this.appletData = [];
+        this.filteredData = [];
+        this.searchInput.addEventListener('input',()=> this.filterApplets());
     }
 
     fetchAppletData(url) {
         fetch(url)
             .then(response => response.json())
-            .then(data => this.renderApplets(data))
+            .then(data => {
+                this.appletData = data;
+                this.filteredData = data;
+                this.renderApplets(this.filteredData);
+            })
             .catch(error => console.error('Error loading applet data:', error));
     }
 
     renderApplets(data) {
+        this.container.innerHTML = '';
         data.forEach(applet => {
             const appletCard = new AppletCard(applet.title, applet.description, applet.link);
             const cardElement = appletCard.createCard();
             this.container.appendChild(cardElement);
         });
     }
+
+    filterApplets(){
+        const query = this.searchInput.value.toLowerCase();
+        this.filteredData = this.appletData.filter(applet =>
+            applet.title.toLowerCase().includes(query) ||
+            applet.description.toLowerCase().includes(query)
+        );
+        this.renderApplets(this.filteredData);
+    }
 }
 
-
-const appletRenderer = new AppletRenderer('applet-container');
-appletRenderer.fetchAppletData('applets.json');
+const appletRenderer = new AppletRenderer('applet-container','searchApplet');
+appletRenderer.fetchAppletData('applet.json');
